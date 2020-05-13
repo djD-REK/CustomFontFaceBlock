855f3e5eab749b33213fda8ab01c4e621f658b0f
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-12 22:02:47 -0400

    Prettier 2

--- __test__/Block.spec.js
+++ __test__/Block.spec.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { mount } from "enzyme";
+import React from "react"
+import { mount } from "enzyme"
 import {
   mockUtils as utils,
   joinClasses,
-} from "@volusion/element-block-utils/test-utils";
-import { block as Block, defaultConfig } from "../src";
+} from "@volusion/element-block-utils/test-utils"
+import { block as Block, defaultConfig } from "../src"
 
-let props;
+let props
 describe("The Block", () => {
   beforeEach(() => {
     props = {
@@ -14,22 +14,22 @@ describe("The Block", () => {
       utils,
       joinClasses,
       queryParams: {},
-    };
-  });
+    }
+  })
   it("renders without errors", () => {
-    mount(<Block {...props} />);
-  });
+    mount(<Block {...props} />)
+  })
   describe("when there is no custom data", () => {
     it("should render the block with the default content", () => {
-      const wrapper = mount(<Block {...props} />);
-      expect(wrapper.text()).toBe(defaultConfig.text);
-    });
-  });
+      const wrapper = mount(<Block {...props} />)
+      expect(wrapper.text()).toBe(defaultConfig.text)
+    })
+  })
   describe("when given custom data", () => {
     it("should render the block using the custom data", () => {
-      const customText = "Custom Block Text";
-      const wrapper = mount(<Block {...props} text={customText} />);
-      expect(wrapper.text()).toBe(customText);
-    });
-  });
-});
+      const customText = "Custom Block Text"
+      const wrapper = mount(<Block {...props} text={customText} />)
+      expect(wrapper.text()).toBe(customText)
+    })
+  })
+})
