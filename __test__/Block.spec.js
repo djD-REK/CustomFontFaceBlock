84d446ea43dbcb028725fefdf41f397f1b933058
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-14 14:31:15 -0400

    Added test

--- __test__/Block.spec.js
+++ __test__/Block.spec.js
@@ -5,6 +5,7 @@ import {
   joinClasses,
 } from "@volusion/element-block-utils/test-utils"
 import { block as Block, defaultConfig } from "../src"
+import { flushToStyleTag } from "aphrodite"
 
 let props
 describe("The Block", () => {
@@ -23,6 +24,19 @@ describe("The Block", () => {
     it("should render the block with the default content", () => {
       const wrapper = mount(<Block {...props} />)
       expect(wrapper.text()).toBe(defaultConfig.text)
+    })
+    it("should use the custom font face to render the content", () => {
+      const wrapper = mount(<Block {...props} />)
+      // Force Aphrodite to output the CSS-in-JS styles in time to test them:
+      flushToStyleTag()
+      const h1Element = wrapper.find("h1").at(0)
+      // Make sure our CSS-in-JS decorator successfully injected the CSS class:
+      expect(h1Element.hasClass(/example_(\w+)/)).toBe(true)
+      // Expect the computed CSS style to match the custom font face:
+      expect(
+        getComputedStyle(h1Element.getDOMNode()).getPropertyValue("font-family")
+      ).toBe("'Dancing Script', cursive")
+      //.toBe(defaultConfig.headingFontSizeString)
     })
   })
   describe("when given custom data", () => {
