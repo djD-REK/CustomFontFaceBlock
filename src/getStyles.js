dd87604c9322be5e50edcf04e6cdcfce55aef426
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-14 21:53:45 -0400

    "bold" // or "normal"

--- src/getStyles.js
+++ src/getStyles.js
@@ -2,6 +2,6 @@ export const getStyles = (blockConfig) => ({
   example: {
     fontSize: "2rem",
     fontFamily: "'Dancing Script', cursive",
-    fontWeight: "normal",
+    fontWeight: "bold", // or "normal"
   },
 })
